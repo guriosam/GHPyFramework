51a4ae7e69f33360f39692dc61eae4d813d42a18
Caio Barbosa <cbvs@ic.ufal.br>
2021-11-22 16:25:33 -0300

    fixing ignore and config file

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ class Main:
 
     def __init__(self):
 
-        json_handler = JSONHandler('C:/Users/gurio/PycharmProjects/GHPyFramework/')
-
         self.config = json_handler.open_json('config.json')
+        json_handler = JSONHandler(self.config['output_path'])
         self.projects = self.config['projects']
 
     def run(self):
@@ -33,28 +32,28 @@ class Main:
             collector.collect_events(project_owner, project_name)
             collector.collect_comments(project_owner, project_name)
 
-    def run_io_mongo(self):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-        for project in self.projects:
-            project_name = project['repo']
-            project_owner = project['owner']
-
-            print(project)
-
-            database = myclient[project_owner + '-' + project_name]
-
-            io = IOMongo(database, project_name)
-
-            io.insert_issues()
-            io.insert_pulls()
-
-            io.insert_events()
-            io.insert_comments()
-            io.insert_commits()
-            io.insert_commits_from_pulls()
+    # def run_io_mongo(self):
+    #     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    #
+    #     for project in self.projects:
+    #         project_name = project['repo']
+    #         project_owner = project['owner']
+    #
+    #         print(project)
+    #
+    #         database = myclient[project_owner + '-' + project_name]
+    #
+    #         io = IOMongo(database, project_name)
+    #
+    #         io.insert_issues()
+    #         io.insert_pulls()
+    #
+    #         io.insert_events()
+    #         io.insert_comments()
+    #         io.insert_commits()
+    #         io.insert_commits_from_pulls()
 
 
 main = Main()
-#main.run()
+main.run()
 #main.run_io_mongo()
