e06b7dcf2b9e427822e853a6908f8f1a3e865f30
Eric Mesquita <ericmesqui2468@gmail.com>
2023-04-22 14:48:42 -0300

    improvements on team size metric

--- metrics/implementation/team_size.py
+++ metrics/implementation/team_size.py
@@ -1,68 +1,46 @@
 from pymongo.database import Database
+from utils.api_call_handler import APICallHandler
 
 class teamSize:
 
     def __init__(self, database: Database = None):
         self.database = database
 
+        self.api_url = 'https://api.github.com/'
+        self.apiCall = APICallHandler()
+
     def teste(self):
-        collections_pull_resquest = self.database['pull_requests']
-        numbers = collections_pull_resquest.find({})
-        collections_comments = self.database['comments']
 
-        prnumbers = set()
-        users_comments = []
-        comments_user = []
-        users_team = {}
-        for number in numbers:
-            prnumbers.add(number['number'])
-            comments = collections_comments.find({})
+        collection_pull_request = self.database['pull_requests']
+        collection_comments = self.database['comments']
+        collection_commits = self.database['commits']
+
+        pr_numbers = collection_pull_request.find({})
+        commits = collection_commits.find({})
+        users_by_pr = {}
+
+        for pr in pr_numbers:
+            pr_number = pr['number']
+            comments = self.database['comments'].find({'issue_number': pr_number})
+            users = set()  # Use a set to avoid duplicates
+
+            #add creator and who merged the pr
+            users.add(pr['user']['login'])
+            if pr['merged_by']:
+                users.add(pr['merged_by']['login'])
+
+
             for comment in comments:
-                if comment['issue_number'] == number['number']:
-                    comments_user.append(comment['user']['login'])
-                    users_team[number['number']] = comments_user
-                    comments_user.clear()
-            users_comments.append(users_team)
-        print(users_comments)
+                users.add(comment['user']['login'])
 
-    def _get_user_info(database, group_key, push_key, push_value):
+            #fetch commit information and extract authors
+            commits_url = pr['commits_url']
+            commit_url = self.apiCall.request(commits_url)
+            if commit_url:
+                for commit in commit_url:
+                    users.add(commit['commit']['author']['name'])
+                    print(commit['commit']['author']['name'], 'in PR', pr_number)
+            users_by_pr[pr_number] = list(users)
 
-        push = {push_key: push_value, 'created_at': '$created_at'}
-        push_commit = {push_key: push_value, 'created_at': '$commit.author.date'}
+        print(users_by_pr)
 
-        if 'commit' in group_key:
-            push = push_commit
-
-        return database.aggregate([
-            {
-                '$group': {
-                    '_id': '$user.login',
-                    group_key: {
-                        '$push': push
-                    }
-                }
-            }, {
-                '$sort': {
-                    'created_at': -1
-                }
-            }
-        ])
-
-"""    def get_team(self):
-
-        collection = self.database['users']
-        users = collection.find({})
-        if 'users_api' not in self.database.name:
-            self.database = self.database['team_users']
-        team = set()
-        collection = self.database
-        for user in users:
-            try:
-                if user['turnover'] != 'Left' or user['turnover'] is None:
-                    if user['interacted_before_180_days'] and user['interacted_within_180_days']:
-                        team.add(user['username'])
-                        collection.insert_one(user)
-            except:
-                print('Errooouuu')
-
-"""
