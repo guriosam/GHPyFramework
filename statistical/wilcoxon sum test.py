ca19ea20b58820a8d7a0ba4cbaadbbbaa82c0703
henriquesantos769 <henripa224444@gmail.com>
2023-04-24 00:44:52 -0300

    Wilcoxon sum test finalizado

--- statistical/wilcoxon sum test.py
+++ statistical/wilcoxon sum test.py
@@ -1,23 +1,33 @@
 import pandas as pd
-from scipy.stats import wilcoxon
+from scipy.stats import wilcoxon, ranksums
 
 
-# Função para realizar o teste de Wilcoxon
 def wilcoxon_test(input_data, metric):
-    # Separar as distribuições "degraded" e "clean"
-    degraded_dist = input_data[input_data["degradation"] == "True"][metric]
-    clean_dist = input_data[input_data["degradation"] == "False"][metric]
+    degraded_dist = input_data[input_data["design_change"] == True][metric]
+    clean_dist = input_data[input_data["design_change"] == False][metric]
 
     # Realizar o teste de Wilcoxon
-    test_stat, pvalue = wilcoxon(degraded_dist, clean_dist)
+    # Inicializar a variável pvalue com None
+    pvalue = None
 
-    # Retornar o resultado
-    metric_data = pd.DataFrame({"metric": [metric], "pvalue": [pvalue]})
+    # Realizar o teste de Wilcoxon apenas se as amostras tiverem tamanho suficiente
+    if degraded_dist.size > 0 and clean_dist.size > 0:
+        test_stat, pvalue =  ranksums(degraded_dist, clean_dist)
+
+    # Verificar se pvalue foi atribuída com sucesso antes de criar o dataframe
+    if pvalue is not None:
+        # Retornar o resultado
+        metric_data = pd.DataFrame({"metric": [metric], "pvalue": [pvalue]})
+    else:
+        # Retornar NaN se pvalue não foi atribuída
+        metric_data = pd.DataFrame({"metric": [metric], "pvalue": [float('nan')]})
+
+
+
     return metric_data
 
-
 # Definir os projetos e métricas
-projects = ["elasticsearch", "presto", "netty", "okhttp", "RxJava", "all"]
+projects = ["elasticsearch", "presto", "netty", "okhttp", "RxJava"]
 metrics = ["number_comments", "number_users", "number_contributors", "number_core_devs", "number_of_patches",
            "words_in_discussion", "words_per_comment_in_discussion", "snippets_size",
            "mean_time_between_replies", "discussion_length"]
@@ -35,15 +45,18 @@ for input_type in input_types:
 
         path = input_type + project
 
+
         # Carregar o conjunto atual de métricas
-        current_metrics = pd.read_csv(path + ".csv")
+        current_metrics = pd.read_csv(path+".csv")
+
 
         # Realizar o teste de Wilcoxon para todas as métricas
         for metric in metrics:
             metric_data = wilcoxon_test(current_metrics, metric)
-            output_data = output_data.append(metric_data)
+            output_data = pd.concat([output_data, metric_data])
 
         # Verificar a significância estatística
         output_data["significant"] = output_data["pvalue"] <= 0.05
         print("Project:", project)
         print(output_data)
+
